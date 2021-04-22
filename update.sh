581dea36ffe5d92c81e7c354182b8f0f2a9e99fa
Sergio Casizzone <jambtc@gmail.com>
2021-04-22 17:31:26 +0200

    update

--- update.sh
+++ update.sh
@@ -5,10 +5,14 @@ echo Updating...
 git stash
 git pull
 
+### Folder rights
+chgrp www-data web/assets
+chmod g+w web/assets/
+
+chgrp www-data runtime
+chmod g+w runtime/
+
 echo Versioning...
 git rev-parse HEAD>version.txt
-
-echo Calling webservice...
-./wsservice.sh
 
 echo Done!
